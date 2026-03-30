7410a29ad23496f895db8ce97468d1fc122f02f4
ElizabethJohney0803 <127989671+ElizabethJohney0803@users.noreply.github.com>
2026-03-29 21:56:53 -0500

    Delete app/core/core directory

--- app/core/core/tests.py
+++
@@ -1,4 +0,0 @@
-from django.test import TestCase
-
-# Create your tests here.
-# Tests will be added as we develop the patient monitoring features
--- app/core/core/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-# Views will be added as we develop the patient monitoring features
